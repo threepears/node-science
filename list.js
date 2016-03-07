d9911278fbe901a3ff5c9365f2b20b6aaf1e7cdf
Michael Conrad <michael@threepears.com>
2016-03-07 15:11:03 -0600

    Create add head, add tail, remove head, remove tail methods on list

--- list.js
+++ list.js
@@ -8,21 +8,48 @@ class Node {
 }
 
 class List {
-  constructor() {
-    this.head = null;
+  constructor () {
+    this.head = null
+    this.tail = null
   }
 
-  add(node) {
-    this.head = node;
+  addHead (node) {
+    this.tail = this.tail || node
+    node.next = this.head
+    this.head = node
+  }
+
+  addTail (node) {
+    if (this.head) {
+      this.tail.next = node
+    } else {
+      this.head = node
+    }
+
+    this.tail = node
+  }
+
+  removeTail () {
+    let current = this.head
+
+    while (current.next !== this.tail) {
+      current = current.next
+    }
+
+    current.next = null
+    this.tail = current
+  }
+
+  removeHead () {
+    this.head = this.head.next
   }
 }
 
-const l = new Node("stuff");
-const m = new Node("things");
-const n = new List;
-n.add(new Node('A'));
-
-
-console.log(l);
-console.log(m);
-console.log(n);
+const list = new List
+list.add(new Node('A'))
+list.add(new Node('B'))
+list.add(new Node('C'))
+list.add(new Node('D'))
+list.add(new Node('E'))
+list.add(new Node('F'))
+console.log(list)
