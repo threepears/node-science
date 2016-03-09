b6564bf8ab94da7b1cbaf1b8aed98fe91a44d3b2
Michael Conrad <michael@threepears.com>
2016-03-09 14:47:08 -0600

    Cleaning up and commenting sort.js

--- sort.js
+++ sort.js
@@ -6,6 +6,7 @@ const util = require('util')
 const arr = [1, 3, 5, 0, 6, 4, 2];
 const arr2 = [4,2,7,6,8,9,5,10,34,23,67,3];
 
+// Create Swap function
 function swap (array, index1, index2) {
   const temp = array[index1];
 
@@ -13,37 +14,39 @@ function swap (array, index1, index2) {
   array[index2] = temp;
 }
 
-// function bubbleSort (array) {
-//   for (let j = 1; j < array.length; j++) {
-//     for (let i = 0; i < array.length - j; i++) {
-//       if (array[i] > array[i+1]) {
-//         swap(array, i, i + 1);
-//         console.log(chalk.red(util.inspect(array)));
-//       }
-//       console.log(chalk.dim(util.inspect(array)));
-//     }
-//   }
-//   return array;
-// }
-
-// If less than the one to the left, move to left; keep moving to left until not true
-
-// function insertionSort (array) {
-//   for (let i = 1; i < array.length; i++) {
-//     for (let j = i; j > 0; j--) {
-//       if (array[j - 1] > array[j]) {
-//         swap(array, j - 1, j);
-
-//         console.log(chalk.red(util.inspect(array)));
-//       } else {
-//         console.log(chalk.dim(util.inspect(array)));
-//       }
-//     }
-//   }
-//   return array;
-// }
+// Implement Bubble Sort
+function bubbleSort (array) {
+  for (let j = 1; j < array.length; j++) {
+    for (let i = 0; i < array.length - j; i++) {
+      if (array[i] > array[i+1]) {
+        swap(array, i, i + 1);
+        console.log(chalk.red(util.inspect(array)));
+      }
+      console.log(chalk.dim(util.inspect(array)));
+    }
+  }
+  return array;
+}
 
 
+// Implement Insertion Sort
+// If previous greater than the one to the right, swap; keep moving to left
+function insertionSort (array) {
+  for (let i = 1; i < array.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (array[j - 1] > array[j]) {
+        swap(array, j - 1, j);
+
+        console.log(chalk.red(util.inspect(array)));
+      } else {
+        console.log(chalk.dim(util.inspect(array)));
+      }
+    }
+  }
+  return array;
+}
+
+// Implement Selection Sort
 function selectionSort (array) {
   for (let i = 0; i < array.length; i++) {
     let index = 0;
@@ -55,8 +58,6 @@ function selectionSort (array) {
         index = j + 1;
       }
     }
-    console.log(i);
-    console.log(index);
     if (index > i) {
       swap(array, i, index);
     }
@@ -64,5 +65,6 @@ function selectionSort (array) {
   return array;
 }
 
-// console.log(bubbleSort(arr2));
+// Testing
+console.log(selectionSort(arr));
 console.log(selectionSort(arr2));
